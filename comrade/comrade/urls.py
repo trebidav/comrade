073fd9a5cde294951c0f655ea27e7c54dfbe1dc0
David Trebicky <davidtrebicky@gmail.com>
2024-03-11 22:13:56 +0100

    feat: django rest framework authentication

--- comrade/comrade/urls.py
+++ comrade/comrade/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from comrade_core import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"tasks", views.TaskViewSet)
+
 
 urlpatterns = [
-    path("comrade/", include("comrade_core.urls")),
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
