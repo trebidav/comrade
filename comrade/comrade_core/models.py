e2db2ea718ed00119ef4e109434f1b842e7d4491
Radim Sückr <radim.suckr@gmail.com>
2024-11-30 13:36:30 +0100

    feat(task): check if user has required permissions before starting task

--- comrade/comrade_core/models.py
+++ comrade/comrade_core/models.py
@@ -1,15 +1,17 @@
 import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     def __str__(self) -> str:
-        return self.username 
-    
-    skills = models.ManyToManyField('Skill', blank=True)
+        return self.username
+
+    skills = models.ManyToManyField("Skill", blank=True)
 
     latitude = models.FloatField(blank=True, default=0)
     longitude = models.FloatField(blank=True, default=0)
@@ -18,11 +20,14 @@ class User(AbstractUser):
 
     def has_skill(self, skill_name):
         return self.skills.filter(name=skill_name).exists()
- 
+
+
 class Skill(models.Model):
     name = models.CharField(max_length=32)
+
     def __str__(self) -> str:
         return self.name
+
 
 class Task(models.Model):
     class Criticality(models.IntegerChoices):
@@ -37,7 +42,7 @@ class Task(models.Model):
         WAITING = 3
         IN_REVIEW = 4
         DONE = 5
-    
+
     def __str__(self) -> str:
         return self.name
 
@@ -46,14 +51,26 @@ class Task(models.Model):
     description = models.CharField(max_length=200, blank=True)
 
     # permissions
-    skill_read = models.ManyToManyField(Skill, related_name='read')
-    skill_write = models.ManyToManyField(Skill, related_name='write')
-    skill_execute = models.ManyToManyField(Skill, related_name='execute')
+    skill_read = models.ManyToManyField(Skill, related_name="read")
+    skill_write = models.ManyToManyField(Skill, related_name="write")
+    skill_execute = models.ManyToManyField(Skill, related_name="execute")
 
     # task state
     state = models.IntegerField(choices=State, default=1, blank=True)
-    owner = models.ForeignKey('comrade_core.User', null=True, blank=True, on_delete=models.RESTRICT, related_name='owned_tasks')
-    assignee = models.ForeignKey('comrade_core.User', null=True, blank=True, on_delete=models.RESTRICT, related_name='assigned_tasks')
+    owner = models.ForeignKey(
+        "comrade_core.User",
+        null=True,
+        blank=True,
+        on_delete=models.RESTRICT,
+        related_name="owned_tasks",
+    )
+    assignee = models.ForeignKey(
+        "comrade_core.User",
+        null=True,
+        blank=True,
+        on_delete=models.RESTRICT,
+        related_name="assigned_tasks",
+    )
 
     # location
     lat = models.FloatField(null=True, blank=True)
@@ -66,29 +83,41 @@ class Task(models.Model):
     # values
     base_value = models.FloatField(blank=True, null=True)
     criticality = models.IntegerField(choices=Criticality, default=1)
-    contribution = models.FloatField(blank=True, null=True, validators=[MaxValueValidator(1.0), MinValueValidator(0.0)])
+    contribution = models.FloatField(
+        blank=True,
+        null=True,
+        validators=[MaxValueValidator(1.0), MinValueValidator(0.0)],
+    )
 
     # time tracking
-    minutes = models.IntegerField(default=10, validators=[MaxValueValidator(480), MinValueValidator(1)])
+    minutes = models.IntegerField(
+        default=10, validators=[MaxValueValidator(480), MinValueValidator(1)]
+    )
     datetime_start = models.DateTimeField(auto_now_add=False, blank=True, null=True)
-    datetime_finish  = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    datetime_finish = models.DateTimeField(auto_now_add=False, blank=True, null=True)
 
-    def start(self, user):
-        self.state = 2
+    def start(self, user: User):
+        has_required_skills = user.skills.filter(
+            id__in=self.skill_execute.all()
+        ).exists()
+        if not has_required_skills:
+            raise ValidationError("User does not have required skills")
+
+        self.state = Task.State.IN_PROGRESS
         self.datetime_start = now()
-        self.owner = user
+        self.assignee = user
         self.save()
 
     def pause(self):
-        if self.state != 2:  # Check if the task is currently in progress
+        if self.state != Task.State.IN_PROGRESS:
             return False
-        self.state = 3  # Set state to WAITING
+        self.state = Task.State.WAITING
         self.save()
 
     def resume(self):
-        if self.state != 3:  # Check if the task is currently in WAITING state
+        if self.state != Task.State.WAITING:
             return False
-        self.state = 2  # Set state back to IN_PROGRESS
+        self.state = Task.State.IN_PROGRESS
         self.save()
 
     def finish(self):
@@ -96,34 +125,42 @@ class Task(models.Model):
         self.save()
 
     def rate(self):
-        if self.state != 2:
+        if self.state != Task.State.IN_PROGRESS:
             return False
         r = Rating()
         r.task = self
         r.save()
-        self.state=4
+        self.state = Task.State.IN_REVIEW
         self.save()
 
     def review(self):
-        if self.state != 4:
+        if self.state != Task.State.IN_REVIEW:
             return False
         r = Review(done=1)
         r.task = self
         r.save()
-        self.state=5
+        self.state = Task.State.DONE
         self.save()
 
+
 class Rating(models.Model):
-    task = models.ForeignKey('comrade_core.Task', default=None, on_delete=models.RESTRICT, blank=True)
-    happiness = models.FloatField(default = 1)
-    time = models.FloatField(default = 1)
-
+    task = models.ForeignKey(
+        "comrade_core.Task", default=None, on_delete=models.RESTRICT, blank=True
+    )
+    happiness = models.FloatField(default=1)
+    time = models.FloatField(default=1)
 
     def __str__(self) -> str:
-        return "Rating of task \""+ self.task + "\""
-    
+        return 'Rating of task "' + self.task + '"'
+
+
 class Review(models.Model):
-    task = models.ForeignKey('comrade_core.Task', default=None, on_delete=models.RESTRICT, blank=True)
-    done = models.FloatField(validators=[MaxValueValidator(1.0), MinValueValidator(0.0)])
+    task = models.ForeignKey(
+        "comrade_core.Task", default=None, on_delete=models.RESTRICT, blank=True
+    )
+    done = models.FloatField(
+        validators=[MaxValueValidator(1.0), MinValueValidator(0.0)]
+    )
+
     def __str__(self) -> str:
-        return "Review of task \""+ self.task + "\""
+        return 'Review of task "' + self.task + '"'
