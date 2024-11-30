057d3db0fff6cc0216731b0cf34b9520ff6aedfc
Radim Sückr <radim.suckr@gmail.com>
2024-11-30 13:33:56 +0100

    feat(admin): add skills to User admin

--- comrade/comrade_core/admin.py
+++ comrade/comrade_core/admin.py
@@ -1,13 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Task, Skill
+from django.contrib.auth.forms import UserChangeForm
+
+from .models import Skill, Task, User
+
+
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class ComradeUserAdmin(UserAdmin):
+    form = UserChangeForm
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("skills",)}),)
+
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['name', 'owner', 'state', 'lat', 'lon', 'respawn', 'respawn_time', 'base_value', 'criticality', 'contribution']
-    list_filter = ['state', 'respawn', 'criticality']
-    search_fields = ['name', 'description']
+    list_display = [
+        "name",
+        "owner",
+        "state",
+        "lat",
+        "lon",
+        "respawn",
+        "respawn_time",
+        "base_value",
+        "criticality",
+        "contribution",
+    ]
+    list_filter = ["state", "respawn", "criticality"]
+    search_fields = ["name", "description"]
 
 
-admin.site.register(User, UserAdmin)
+class SkillInline(admin.StackedInline):
+    model = Skill
+
+
+admin.site.register(User, ComradeUserAdmin)
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Skill)
