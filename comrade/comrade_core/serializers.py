6254812e73d3517a16a02b7c44f72f3e2587e95d
David Trebicky <davidtrebicky@gmail.com>
2026-03-25 17:30:49 +0100

    refactor: remove shadowed and unused serializers

--- comrade/comrade_core/serializers.py
+++ comrade/comrade_core/serializers.py
@@ -1,27 +1,6 @@
 from comrade_core.models import Task, User, Review, Skill, TutorialTask, TutorialPart, TutorialQuestion, TutorialAnswer, TutorialProgress
-from django.contrib.auth.models import Group
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "groups"]
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ["url", "name"]
-
-
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Task
-        fields = ["title"]
-
-User = get_user_model()
 
 class UserDetailSerializer(serializers.ModelSerializer):
     skills = serializers.StringRelatedField(many=True)
